bf4435afb2cd6bca5546d7c278b4c128934ec5a0
leticia <leticiaccsampaio@gmail.com>
2022-05-08 17:08:37 -0300

    commit first module of alura cpp course

---
+++ module01/jogodaadivinhacao.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+using namespace std;
+
+int main (){
+    cout << "*************************************" << endl;
+    cout << "** * Welcome to the guess game! * **" << endl;
+    cout << "*************************************" << endl;
+
+    int numero_secreto = 42;
+    cout << "The secret number is " << numero_secreto << ". Don't tell anyone." << endl;
+}
